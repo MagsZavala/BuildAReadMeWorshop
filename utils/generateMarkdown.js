da61447bf5604430b44509f8e6574a39e61bf12f
Magali Zavala <158116338+MagsZavala@users.noreply.github.com>
2024-07-14 15:51:34 -0700

    render license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,64 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+    if (license === 'None') {
+      return '';
+    }
+    return `![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  }
+  
+  // Function that returns the license link
+  function renderLicenseLink(license) {
+    if (license === 'None') {
+      return '';
+    }
+    return `\n* [License](#license)\n`;
+  }
+  
+  // Function that returns the license section of README
+  function renderLicenseSection(license) {
+    if (license === 'None') {
+      return '';
+    }
+    return `## License
+  
+  This project is licensed under the ${license} license.`;
+  }
+  
+  // Function to generate markdown for README
+  function generateMarkdown(data) {
+    return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ${renderLicenseLink(data.license)}
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions about the repository, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}).
+  
+  ${renderLicenseSection(data.license)}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
