df79ba401a9f4d9d0097a4dfb0a4e9e0573cbf9b
Magali Zavala <158116338+MagsZavala@users.noreply.github.com>
2024-07-14 15:51:56 -0700

    array of questions

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,79 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description explaining the what, why, and how of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use your project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute to your project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How do you run tests for your project?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('README.md file has been generated successfully!');
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    const markdownContent = generateMarkdown(answers);
+    writeToFile('README.md', markdownContent);
+  } catch (error) {
+    console.error('Error creating README file:', error);
+  }
+}
 
 // Function call to initialize app
 init();
+
